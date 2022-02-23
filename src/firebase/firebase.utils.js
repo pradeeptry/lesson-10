2a692720cc42af204240c9aabfef47a0784c7f6d
pradeep logicsquare <pradeep@logic-square.com>
2022-02-23 19:52:29 +0530

    from now start redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config ={
+        apiKey: "AIzaSyCNDmQPsUwtBGP-hoCFIc1mVyCPw77vAno",
+        authDomain: "webcommauth.firebaseapp.com",
+        projectId: "webcommauth",
+        storageBucket: "webcommauth.appspot.com",
+        messagingSenderId: "492345057915",
+        appId: "1:492345057915:web:3745e687d551192a7ef8de",
+        measurementId: "G-BNZNQV6TTP"
 };
+
 
 firebase.initializeApp(config);
 
